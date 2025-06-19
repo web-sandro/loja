d1339069342f2ae875494fd3a9975e9512560e6a
Sandro Donizeti Gomes <129443003+web-sandro@users.noreply.github.com>
2025-06-19 15:02:28 -0300

    OK-OK

--- tests/usuario.js
+++
@@ -1,9 +0,0 @@
-const Usuario = require('../models/usuario');
-
-(async () => {
-    const usuario1 = new Usuario({nome: 'Luiz', apelido: 'lro', senha: '123', email: 'lro@gmail.com'});
-
-    await usuario1.save();
-
-    console.log(usuario1)
-})();
